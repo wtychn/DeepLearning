61a2562eae1b3e3d8d19a7d0c90fcd84216fd991
wtychn <452443579@qq.com>
2021-04-28 11:15:53 +0800

    feat: add net train jupyter file

--- test.py
+++ test.py
@@ -8,9 +8,14 @@ from DBtest.Move import Move
 #     it.treatment(srcDir, targetDir, isDehaze=False, isRepair=False, isSave=True)
 
 
-m = Move()
-# m.copy('D:\\OneDrive - csu.edu.cn\\研究课题\\铁水图像视频\\part1', 'DBtest\\img\\data2')
-for i in range(10):
-    srcDir = 'DBtest/img/test/' + str(i) + '/'
-    targetDir = 'DBtest/img/val/' + str(i) + '/'
-    m.cut(srcDir, targetDir, proportion=0.2)
+# m = Move()
+# # m.copy('D:\\OneDrive - csu.edu.cn\\研究课题\\铁水图像视频\\part1', 'DBtest\\img\\data2')
+# for i in range(10):
+#     srcDir = 'DBtest/img/test/' + str(i) + '/'
+#     targetDir = 'DBtest/img/val/' + str(i) + '/'
+#     m.cut(srcDir, targetDir, proportion=0.2)
+
+
+k = [2, 3, 4, 5]
+params = [{'params': md.parameters()} for i in range(10)
+          if i in k]
