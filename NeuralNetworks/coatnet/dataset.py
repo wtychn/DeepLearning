77937f3f07158744b99109b3b023f7e6127ec7a0
wtychn <452443579@qq.com>
2022-04-10 21:09:52 +0800

    feat: add fusion datasets

--- NeuralNetworks/coatnet/dataset.py
+++ NeuralNetworks/coatnet/dataset.py
@@ -3,7 +3,9 @@ import random
 from datetime import datetime
 
 import numpy as np
+import pandas as pd
 from PIL import Image
+import cv2
 
 import torch
 from torch.utils.data import Dataset
@@ -106,3 +108,34 @@ class RegressionImageDataset(Dataset):
 
     def __len__(self):
         return self.number_image
+
+
+class FusionDataset(Dataset):
+
+    def __init__(self, data_dir, img_dir):
+        super(FusionDataset, self).__init__()
+
+        dictionary_array = np.array([])
+        flag = 0
+
+        for filename in os.listdir(data_dir):
+            data_path = os.path.join(data_dir, filename)
+            df = pd.read_csv(data_path, header=None)
+            for index, row in df.iterrows():
+                img_path = os.path.join(os.path.join(img_dir, str(flag)), row[0] + ".jpg")
+                if os.path.exists(img_path):
+                    dictionary = {
+                        "image": cv2.imread(img_path).reshape(3, 224, 224),
+                        "x": row[2:].to_numpy(),
+                        "y": flag
+                    }
+                    dictionary_array = np.append(dictionary_array, dictionary)
+            flag += 1
+
+            self.datas = dictionary_array
+
+    def __getitem__(self, index):
+        return self.datas[index]
+
+    def __len__(self):
+        return len(self.datas)
